973617476c1f2d5aad9a8d89c453aabf7fd95010
Tran Phuoc Cuong <cuongtp@hybrid-technologies.co.jp>
2023-03-29 11:53:27 +0700

    feat: remove file

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Dashboard from "./pages/dashboard/Dashboard";
-
-function App() {
-  return <Dashboard></Dashboard>;
-}
-
-export default App;
--- src/components/card-detail/CardDetail.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Modal from "../modal/Modal";
-
-const CardDetail = () => {
-  return <Modal></Modal>;
-};
-
-export default CardDetail;
